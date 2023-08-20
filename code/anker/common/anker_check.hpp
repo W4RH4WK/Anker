df090240b3c27fea205249d6192ea1be258b8036
Alex Hirsch <w4rh4wk@bluephoenix.at>
2023-08-20 10:50:49 +0200

    Add ANKER_CHECK_NUM_CAST

--- code/anker/common/anker_check.hpp
+++ code/anker/common/anker_check.hpp
@@ -4,6 +4,7 @@
 
 #define ANKER_ENABLE_CHECKS 1
 #define ANKER_ENABLE_CHECKS_INBOUNDS 1
+#define ANKER_ENABLE_CHECKS_NUM_CAST 1
 
 #if ANKER_ENABLE_CHECKS
 #define ANKER_CHECK(condition) \
@@ -21,4 +22,11 @@
 	ANKER_CHECK(static_cast<::std::size_t>(index) < static_cast<::std::size_t>(size))
 #else
 #define ANKER_CHECK_INBOUNDS(index, size)
+#endif
+
+#if ANKER_ENABLE_CHECKS_NUM_CAST
+#define ANKER_CHECK_NUM_CAST(toType, value) \
+	ANKER_CHECK(::std::numeric_limits<toType>::min() <= value && value <= ::std::numeric_limits<toType>::max())
+#else
+#define ANKER_CHECK_NUM_CAST(toType, value)
 #endif
