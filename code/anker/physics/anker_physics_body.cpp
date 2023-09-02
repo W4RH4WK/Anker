1c0eb9216ca5857ca1b0bc68552944356d47bd37
Alex Hirsch <w4rh4wk@bluephoenix.at>
2023-09-02 20:07:29 +0200

    Provide accessor for PhysicsBody transform

--- code/anker/physics/anker_physics_body.cpp
+++ code/anker/physics/anker_physics_body.cpp
@@ -2,24 +2,4 @@
 
 #include <anker/editor/anker_edit_widget_drawer.hpp>
 
-namespace Anker {
-
-bool serialize(EditWidgetDrawer& draw, PhysicsBody& body)
-{
-	bool changed = false;
-
-	Vec2 position = body.body->GetPosition();
-	float rotation = body.body->GetTransform().q.GetAngle() / Degrees;
-
-	changed |= draw.field("position", position);
-	changed |= draw.field("rotation", rotation);
-
-	if (changed) {
-		body.body->SetTransform(position, rotation * Degrees);
-		body.body->SetAwake(true);
-	}
-
-	return changed;
-}
-
-} // namespace Anker
+namespace Anker {} // namespace Anker
--- code/anker/physics/anker_physics_body.hpp
+++ code/anker/physics/anker_physics_body.hpp
@@ -1,15 +1,23 @@
 #pragma once
 
+#include <anker/core/anker_transform.hpp>
+
 namespace Anker {
 
 struct PhysicsBody {
-	b2Body* body;
-};
+	Transform2D transform() const { return body->GetTransform(); }
+	void setTransform(const Transform2D& transform)
+	{
+		body->SetTransform(transform.position, transform.rotation);
+		body->SetAwake(true);
+	}
 
-class EditWidgetDrawer;
-bool serialize(EditWidgetDrawer&, PhysicsBody&);
+	b2Body* body = nullptr;
+};
 
 } // namespace Anker
 
 REFL_TYPE(Anker::PhysicsBody)
+REFL_FUNC(transform, property())
+REFL_FUNC(setTransform, property())
 REFL_END
