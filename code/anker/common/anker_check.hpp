f9b3ad31175d1e862854c8c88a0e49f673261a3b
Alex Hirsch <w4rh4wk@bluephoenix.at>
2023-11-02 17:26:40 +0100

    ANKER_CHECK cannot be disabled

--- code/anker/common/anker_check.hpp
+++ code/anker/common/anker_check.hpp
@@ -2,9 +2,6 @@
 
 #include "anker_log.hpp"
 
-#define ANKER_CHECK_ENABLED 1
-
-#if ANKER_CHECK_ENABLED
 // ANKER_CHECK is similar to ANKER_ASSERT, but instead of terminating the
 // program we log and return from the current function. ret can be omitted for
 // functions without return values (void).
@@ -15,6 +12,3 @@
 			return ret; \
 		} \
 	} while (0)
-#else
-#define ANKER_CHECK(condition, ret)
-#endif
