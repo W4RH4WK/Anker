99d107a7e948e3b564190e483bf70d0ce62d5b29
Alex Hirsch <w4rh4wk@bluephoenix.at>
2023-12-14 12:32:18 +0100

    Audio: Add playEffect

--- code/anker/audio/anker_audio_system.cpp
+++ code/anker/audio/anker_audio_system.cpp
@@ -18,11 +18,21 @@ AudioSystem::AudioSystem()
 	if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 4096) != 0) {
 		ANKER_ERROR("Mix_OpenAudio failed: {}", Mix_GetError());
 	}
+
+	int allocatedChannels = Mix_AllocateChannels(EffectChannelCount);
+	if (allocatedChannels != EffectChannelCount) {
+		ANKER_ERROR("Mix_AllocateChannels: Could not allocate desired number of effect channels allocatedChannels={} "
+		            "EffectChannelCount={}",
+		            allocatedChannels, EffectChannelCount);
+	}
 }
 
 AudioSystem::~AudioSystem()
 {
 	m_music = nullptr;
+	for (auto& effect : m_effects) {
+		effect = nullptr;
+	}
 	Mix_Quit();
 }
 
@@ -50,6 +60,22 @@ void AudioSystem::stopMusic(float fadeTime)
 		Mix_FadeOutMusic(int(fadeTime * 1000.0f));
 	}
 	m_music = nullptr;
+}
+
+void AudioSystem::playEffect(AssetPtr<AudioTrack> effect, float volume)
+{
+	ANKER_CHECK(effect);
+
+	int channel = Mix_PlayChannel(-1, *effect, 0);
+	if (channel < 0) {
+		ANKER_WARN("Not enough audio channels available");
+		return;
+	}
+
+	Mix_Volume(channel, int(std::clamp(volume, 0.0f, 1.0f) * MIX_MAX_VOLUME));
+
+	ANKER_CHECK(channel < m_effects.size());
+	m_effects[channel] = effect;
 }
 
 } // namespace Anker
--- code/anker/audio/anker_audio_system.hpp
+++ code/anker/audio/anker_audio_system.hpp
@@ -18,8 +18,13 @@ class AudioSystem {
 	void playMusic(AssetPtr<AudioStream>, float fadeTime = 0.5f);
 	void stopMusic(float fadeTime = 0.5f);
 
+	void playEffect(AssetPtr<AudioTrack>, float volume = 1);
+
   private:
+	static constexpr int EffectChannelCount = 64;
+
 	AssetPtr<AudioStream> m_music;
+	std::array<AssetPtr<AudioTrack>, EffectChannelCount> m_effects = {};
 };
 
 } // namespace Anker
